ad78717e4451e1253b7cc5acd04d0b44aa611a6d
BloodBath <th11rushbase1234.com@gmail.com>
2026-05-23 12:10:36 +0530

    Delete src directory

--- src/services/anixplay.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Decommissioned in favor of Anify Protocol V13.0
- */
-export async function getEpisodeCounts() {
-  return { sub: 0, dub: 0 };
-}
--- src/services/consumet.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Decommissioned in favor of Anixplay Protocol V11.0
- */
-export async function getEpisodeCounts() {
-  return { sub: 0, dub: 0 };
-}
