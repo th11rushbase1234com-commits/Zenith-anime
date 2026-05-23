4ec1d6479272ac470947976e7ff8c706dcf50357
BloodBath <th11rushbase1234.com@gmail.com>
2026-05-23 12:25:49 +0530

    Delete src directory

--- src/app/lib/mock-data.ts
+++
@@ -1,3 +0,0 @@
-
-// Removed in favor of real Jikan API data and Firestore persistence.
-export const INITIAL_ANIME = [];
--- src/services/anixplay.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Decommissioned in favor of Anify Protocol V1.0
- */
-export async function getEpisodeCounts() {
-  return { sub: 0, dub: 0 };
-}
